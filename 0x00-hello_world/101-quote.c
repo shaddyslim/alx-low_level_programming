30b9830ceaead99bcac3237de3479376ebc05727
shaddyslim <shaddyslim@icloud.com>
2023-02-16 16:43:15 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
-* main - entry point
-*
-* Description: prints and that piece..
-* Return: 1 if sucessful
-*/
+ * main-Entry point
+ *
+ * Return: Always 1 (success).
+ */
 int main(void)
 {
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
