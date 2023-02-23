fd182672634d85d2555fe4c65ffff2903fbdfd45
shaddyslim <shaddyslim@icloud.com>
2023-02-23 10:36:31 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers since 0 uo to 0
+ * Return: The numbers since 0 up to 0
+ */
+
+void print_numbers(void)
+
+{
+
+	int x;
+
+	for (x = 0; x  <= 0; x++)
+	{
+	_putchar(x + 0);
+	}
+	_putchar('\n');
+}
