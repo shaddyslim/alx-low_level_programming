4c6efa977f624990cb7c7edb9c51dc3775c3ebbb
shaddyslim <shaddyslim@icloud.com>
2023-03-06 12:03:07 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,17 +1,31 @@
 #include "main.h"
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (success)
- */
-char *_strchr(char *s, char c)
+*   _strspn - return length of string that matches values consistently
+*    @s: string to search
+*     @accept: target matches
+*      Return: number of bytes consecutively matched
+*/
+
+unsigned int _strspn(char *s, char *accept)
 {
-	int i = 0;
-	for (; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
+int i = 0, j;
+int matches = 0;
+
+while (s[i] != '\0') /*iterate through string*/
+{
+
+for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+{
+if (s[i] == accept[j]) /*record & break at first match*/
+{
+matches++;
+break;
+}
+if (accept[j + 1] == '\0' && s[i] != accept[j])
+return (matches);/*return if idx doesn't match*/
+}
+i++;
+}
+return (matches); /* return num if all match till end */
+
 }
