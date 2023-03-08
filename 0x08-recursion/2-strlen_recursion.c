0fd196e2df9215513d2db443e05eebc1a039357f
shaddyslim <shaddyslim@icloud.com>
2023-03-08 06:14:56 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string.
+ * _strlen_recursion - Returns the length of a string.
  * @s: The string to be measured.
  *
- * Return: the length of the string.
+ * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
@@ -15,3 +15,4 @@ int _strlen_recursion(char *s)
 		len++;
 		len += _strlen_recursion(s + 1);
 	}
+}
