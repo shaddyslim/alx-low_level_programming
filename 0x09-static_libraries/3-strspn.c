8a28db15bd45d2d58489eb375fc809e855237c05
shaddyslim <shaddyslim@icloud.com>
2023-03-10 15:04:50 -0800

    update

--- 0x09-static_libraries/3-strspn.c
+++ 0x09-static_libraries/3-strspn.c
@@ -1,28 +1,14 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: initial segment.
- * @accept: accepted bytes.
- * Return: the number of accepted bytes.
+ * _islower - prints 1 or 0 depending on input
+ * @c: first parameter
+ *
+ * Description: prints all lowercase letters
+ * Return: Always(0).
  */
-unsigned int _strspn(char *s, char *accept)
-{
-	unsigned int i, j, bool;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-			{
-				bool = 0;
-				break;
-			}
-		}
-		if (bool == 1)
-			break;
-	}
-	return (i);
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
 }
